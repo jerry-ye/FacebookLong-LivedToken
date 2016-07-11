de7d5836a04d5805895355987d7cffc3fe65d5c8
Jerry <jeriyeh@gmail.com>
2016-07-11 17:11:31 +0800

    add a function easy to fetch the long lived token

--- src/Token/FacebookToken.php
+++ src/Token/FacebookToken.php
@@ -12,6 +12,7 @@ class FacebookToken {
     private $app_id;
     private $app_url;
     private $app_secret;
+    private $page_access_tokens = array();
 
     /**
      * init facebook token.
@@ -27,12 +28,20 @@ class FacebookToken {
         $this->short_lived_access_token = $short_lived_access_token;
     }
 
+    public function getLongLivedAccessToken() {
+        return $this->makeAsPermanentAccessToken($this->getLongAccessToken($this->getCode()));
+    }
+
     public function setAccessToken($short_lived_access_token) {
         $this->short_lived_access_token = $short_lived_access_token;
     }
 
     public function getAccessToken() {
         return $this->long_lived_access_token;
+    }
+
+    public function getPageAccessTokens() {
+        return $this->page_access_tokens;
     }
 
     /**
@@ -57,6 +66,9 @@ class FacebookToken {
      * @return type
      */
     function getLongAccessToken($code) {
+        if (!isset($code->code)) {
+            return FALSE;
+        }
         $exchange_access_token_url = self::FB_API_URL . 'oauth/access_token';
         $res = $this->client->request('GET', $exchange_access_token_url, [
             'query' => [
@@ -79,15 +91,24 @@ class FacebookToken {
      * @return type
      */
     function makeAsPermanentAccessToken($long_lived_token) {
+        if (!isset($long_lived_token->access_token)) {
+            return FALSE;
+        }
+        $this->long_lived_access_token = $long_lived_token->access_token;
         $account_access_token_url = self::FB_API_URL . 'me/accounts';
         $res = $this->client->request('GET', $account_access_token_url, [
             'query' => [
-                'access_token' => $long_lived_token->access_token,
+                'access_token' => $this->long_lived_access_token,
                 'client_id' => $this->app_id,
                 'redirect_uri' => $this->app_url
             ]
         ]);
-        return $res->getStatusCode() == 200 ? json_decode($res->getBody()) : FALSE;
+
+        if ($res->getStatusCode() == 200) {
+            $res = json_decode($res->getBody());
+            $this->page_access_tokens = $res->data;
+        }
+        return $this->long_lived_access_token;
     }
 
 }
